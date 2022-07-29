bbdc3bcc2a240c5c0bf119ee2c0783478e75a074
Omar Hamad <etahamad@icloud.com>
2022-07-29 14:52:48 +0200

    str_index

---
+++ str_index.py
@@ -0,0 +1,15 @@
+selfish = '01234567'
+        #  01234567
+
+print(selfish[0])
+print(selfish[7])
+
+# [start:stop:step]
+print(selfish[:5]) 
+print(selfish[::1])
+print(selfish[1:]) # start at 1, stop at end
+print(selfish[0:8:2]) # step or stepover: str = 01234567 , if step = 2, str = 0246
+print(selfish[::-1]) # reverse string
+print(selfish[-1])
+
+# vscdoe note: to rename similar names: mark the word then click f2 and type the new variable name
