e40440961c9ee029d685abacab330b34d6fbad92
Omar Hamad <etahamad@icloud.com>
2022-07-29 17:43:13 +0200

    exercise password checker

---
+++ ex_password_checker.py
@@ -0,0 +1,5 @@
+username = input("Enter your username: ")
+password = input("Enter your password: ")
+
+passwordInStars = "*" * len(password)
+print(f"Your username is {username} and your password is {passwordInStars}")
