91a1d95953c366d22f48d0c327ff6d98cb5aafcd
Omar Hamad <etahamad@icloud.com>
2022-07-29 19:33:12 +0200

    matrix

---
+++ matrix.py
@@ -0,0 +1,10 @@
+# using this list: 
+basket = [
+    "Banana", ["Apples", ["Oranges"], "Blueberries"]
+];
+# access "Oranges" and print it:
+# draft, let's explain the answer
+print(basket[0])
+print(basket[1])
+print(basket[1][1]) # this prints the list inside the list: ["Oranges"]
+print(basket[1][1][0]) # this pritns the first element in the list: Oranges (this is the answer)
