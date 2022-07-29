74528f863da0474f1192964fcad0fd2be9e41212
Omar Hamad <etahamad@icloud.com>
2022-07-29 20:19:46 +0200

    list methods 2

---
+++ list_methods_2.py
@@ -0,0 +1,13 @@
+basket = ['a', 'b', 'c', 'd', 'e', 'd']
+#          0    1    2    3    4    5
+
+print(basket.index('d', 0 , 4)) 
+
+print('d' in basket) 
+print('f' in basket) 
+
+print('o' in 'Omar')
+print('o' in 'omar')
+
+print(basket.count('d'))
+print(basket.count('f'))
