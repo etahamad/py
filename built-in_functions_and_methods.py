577a8c7d7e844dc699beb05ccef85115865e7eb6
Omar Hamad <etahamad@icloud.com>
2022-07-29 15:19:13 +0200

    built-in functions and methods

---
+++ built-in_functions_and_methods.py
@@ -0,0 +1,24 @@
+name = 'hamad'
+
+print(name[:])
+print(name[:len(name)])
+
+quote = 'if you are reading this you are stalking me'
+
+print(quote.capitalize()) 
+print(quote.upper())
+
+print(quote.find('you')) # returns the index of the first occurrence of the substring
+print(quote.replace('stalking me', 'a good person'))
+
+print(quote)
+
+'''
+still the same? why?
+strings are immutable, they can not be changed
+we can overwrite them if we want but we don't change them
+'''
+
+# let's try something else
+quote2 = quote.replace('stalking me', 'a good person in quote 2')
+print(quote2)
