fc5bbd0c6638c7143117acac7558eec35113e3ae
Omar Hamad <etahamad@icloud.com>
2022-07-30 11:45:04 +0200

    dict methods 2

--- dict.py
+++ dict.py
@@ -1,6 +1,6 @@
 dictionary = {
     'name': 'Omar',
-    'age': '21',
+    'age': 21,
     'city': ['10th of Ramadan', 'Cairo', 'Alexandria'],
     'country': 'Egypt'
 
@@ -9,13 +9,13 @@ dictionary = {
 myList = [
     {
     'name': 'Omar',
-    'age': '21',
+    'age': 21,
     'city': ['10th of Ramadan', 'Cairo', 'Alexandria'],
     'country': 'Egypt'
     },
     {
     'name': 'Potato',
-    'age': '21',
+    'age': 21,
     'city': ['New York', 'Los Angeles', 'Chicago'],
     'country': 'Egypt'
     }
@@ -33,16 +33,24 @@ user = {
     'username': 'etahamad',
     'first': 'Omar',
     'last': 'Hamad',
-    'age': '21',
+    'age': 21,
     'city': 'Cairo',
     'country': 'Egypt'
 }
 
-user2 = dict(username='etahamad', first='Omar', last='Hamad', age='21', city='Cairo', country='Egypt') # not common in python
+user2 = dict(username='etahamad', first='Omar', last='Hamad', age=21, city='Cairo', country='Egypt') # not common in python
 
 # print(dictionary['city'][0])
 # print(myList[1]['city'][2])
 # print(myDictWithCustomKeys[123])
-print(user.get('checkget', 'Not Found'))
-print(user.get('username', 'Not Found'))
-print(user2)
+# print(user.get('checkget', 'Not Found'))
+# print(user.get('username', 'Not Found'))
+# print(user2)
+# print('username' in user.keys())
+# print('Omar' in user.values())
+# print(user.items())
+# user.clear()
+# print(user)
+print(user.pop('username'))
+print(user.update(age=69))
+print(user)
