929ad06b60d93388e643d7575c2dde5585dea273
Omar Hamad <etahamad@icloud.com>
2022-07-30 12:22:21 +0200

    sets 1

---
+++ set_1.py
@@ -0,0 +1,12 @@
+mySet = {1, 2, 3, 4, 5, 5}
+
+mySet.add(6)
+mySet.add(2)
+print(mySet)
+print(1 in mySet)
+print(len(mySet))
+print(list(mySet))
+
+myList = [1, 2, 3, 4, 5, 5]
+mySet2 = set(myList)
+print(mySet2)
