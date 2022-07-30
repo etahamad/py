d8589e96f55dcf0edebd13a74f5a6bb9ec741635
Omar Hamad <etahamad@icloud.com>
2022-07-30 09:54:56 +0200

    dictionary

---
+++ dict.py
@@ -0,0 +1,25 @@
+dictionary = {
+    'name': 'Omar',
+    'age': '21',
+    'city': ['10th of Ramadan', 'Cairo', 'Alexandria'],
+    'country': 'Egypt'
+
+}
+
+myList = [
+    {
+    'name': 'Omar',
+    'age': '21',
+    'city': ['10th of Ramadan', 'Cairo', 'Alexandria'],
+    'country': 'Egypt'
+    },
+    {
+    'name': 'Potato',
+    'age': '21',
+    'city': ['New York', 'Los Angeles', 'Chicago'],
+    'country': 'Egypt'
+    }
+]
+
+print(dictionary['city'][0])
+print(myList[1]['city'][2])
