ce11e3600c811a7d6887046a5b152d01a6233c76
Omar Hamad <etahamad@icloud.com>
2022-07-30 09:02:44 +0200

    lists-3 exercise
    https://replit.com/@aneagoie/lists-3#main.py

---
+++ ex_list_conversion.py
@@ -0,0 +1,7 @@
+friends = ['Simon', 'Patty', 'Joy', 'Carrie', 'Amira', 'Chu']
+
+new_friend = ['Stanley']
+
+allFriends = friends + new_friend # or use friends.extend(new_friend)
+allFriends.sort()
+print(allFriends)
