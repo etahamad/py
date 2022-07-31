817067d501f0a0dbe63f2b784847b89a088ba8d0
Omar Hamad <etahamad@icloud.com>
2022-07-31 17:45:41 +0200

    scope

---
+++ Python Basics II/scope.py
@@ -0,0 +1,10 @@
+num = 1
+
+def parent():
+    num = 2
+    def child():
+        return num, sum # sum is for testing built-in functions inside a scope
+    return child()
+
+print(parent())
+print(num)
