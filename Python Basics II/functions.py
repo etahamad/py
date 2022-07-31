621fa2939f8da4c3fde44c852c184c0b24b9f98e
Omar Hamad <etahamad@icloud.com>
2022-07-31 13:16:27 +0200

    make the func easy to understand

--- Python Basics II/functions.py
+++ Python Basics II/functions.py
@@ -1,6 +1,6 @@
 def sum(num1, num2):
-    def secondSum(num1, num2):
-        return num1 + num2
+    def secondSum(n1, n2):
+        return n1 + n2
     return secondSum(num1, num2)
 
 total = sum(10,20)
