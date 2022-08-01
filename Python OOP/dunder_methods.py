e1a0e51a7261a01cb8d95f06e79f7b68b315dd46
Omar Hamad <etahamad@icloud.com>
2022-08-01 10:12:50 +0200

    dunder methods

---
+++ Python OOP/dunder_methods.py
@@ -0,0 +1,41 @@
+# By reading the python documentation, add 3 more magic/dunder methods of your choice to this Toy class.
+class Toy():
+    def __init__(self, color, age):
+        self.color = color
+        self.age = age
+        self.my_dict = {
+            'name': 'Yoyo',
+            'has_pets': False,
+        }
+
+    def __str__(self):
+        return f"{self.color}"
+
+    def __len__(self):
+        return 5
+
+    def __del__(self):
+        return "deleted"
+
+    def __call__(self):
+        return('yes??')
+
+    def __getitem__(self, i):
+        return self.my_dict[i]
+
+    def __setitem__(self, i, v):
+        self.my_dict[i] = v
+
+    def __delitem__(self, i):
+        del self.my_dict[i]
+
+    def __contains__(self, i):
+        return i in self.my_dict
+
+
+action_figure = Toy('red', 0)
+print(action_figure.__str__())
+print(str(action_figure))
+print(len(action_figure))
+print(action_figure())
+print(action_figure['name'])
