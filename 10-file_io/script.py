4e86a95c8557258fd6235dc284a66160f62286ae
Omar Hamad <etahamad@icloud.com>
2022-08-04 22:04:57 +0200

    i/o: errors

--- 10-file_io/script.py
+++ 10-file_io/script.py
@@ -1,2 +1,16 @@
-with open('./app/sad.txt', 'r') as f:
-    print(f.read())
+try:
+    with open('./app/sad.txt', 'r') as f:
+        print(f.read())
+except FileNotFoundError:
+    print('File not found')
+except PermissionError:
+    print('Permission denied')
+except IOError:
+    print('IO error')
+except Exception as e:
+    raise e
+else:
+    print('File read successfully')
+finally:
+    print('File closed')
+print('Program ended')
