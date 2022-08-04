ead66c12508c70f98a3a054079c482450927a771
Omar Hamad <etahamad@icloud.com>
2022-08-04 22:33:32 +0200

    problem_solving: q4

---
+++ problem_solving/q4.py
@@ -0,0 +1,13 @@
+# Write a program which accepts a sequence of comma-separated numbers from console
+# and generate a list and a tuple which contains every number.
+# Suppose the following input is supplied to the program:
+# 34,67,55,33,12,98
+
+def main():
+    numbers = input("Enter a sequence of numbers: ")
+    numbers = numbers.split(",")
+    print("List:", numbers)
+    print("Tuple:", tuple(numbers))
+
+
+main()
