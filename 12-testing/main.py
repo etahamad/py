da274938cc5a28a152bc80fe5991b29786ecd65f
Omar Hamad <etahamad@icloud.com>
2022-08-05 14:33:25 +0200

    writing unit tests

--- 12-testing/main.py
+++ 12-testing/main.py
@@ -1,5 +1,8 @@
-def do_stuff(n):
+def do_stuff(n=0):
     try:
-        return int(n) * 2
+        if n:
+            return int(n) * 2
+        else:
+            return 'Please enter a number'
     except ValueError as e:
         return e
--- 12-testing/test1.py
+++ 12-testing/test1.py
@@ -11,7 +11,23 @@ class TestMain(unittest.TestCase):
     def test_do_stuff_2(self):
         test_param = 'anything'
         result = main.do_stuff(test_param)
-        self.assertTrue(isinstance(result, ValueError))
+        self.assertIsInstance(result, ValueError)
+
+    def test_do_stuff_3(self):
+        test_param = None
+        result = main.do_stuff(test_param)
+        self.assertEqual(result, 'Please enter a number')
+
+    def test_do_stuff_4(self):
+        test_param = ''
+        result = main.do_stuff(test_param)
+        self.assertEqual(result, 'Please enter a number')
+
+    def test_do_stuff_5(self):
+        test_param = 0
+        result = main.do_stuff(test_param)
+        self.assertEqual(result, 'Please enter a number')
 
 
-unittest.main()
+if __name__ == '__main__':
+    unittest.main()
