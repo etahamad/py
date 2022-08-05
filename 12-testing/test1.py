054412e45a9fa4abca5a0379f7372dc6d9a4edc0
Omar Hamad <etahamad@icloud.com>
2022-08-05 14:31:12 +0200

    writing unit tests 2

--- 12-testing/test1.py
+++ 12-testing/test1.py
@@ -3,6 +3,9 @@ import main
 
 
 class TestMain(unittest.TestCase):
+    def setUp(self):
+        print('about to run a test')
+
     def test_do_stuff(self):
         test_param = 10
         result = main.do_stuff(test_param)
@@ -27,6 +30,9 @@ class TestMain(unittest.TestCase):
         test_param = 0
         result = main.do_stuff(test_param)
         self.assertEqual(result, 'Please enter a number')
+
+    def tearDown(self):
+        print('cleaning up after the test')
 
 
 if __name__ == '__main__':
