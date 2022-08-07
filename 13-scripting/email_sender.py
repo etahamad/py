954a45e1fe5509a5129cd468e1ea52a98d2a91fa
Omar Hamad <etahamad@icloud.com>
2022-08-07 20:07:41 +0200

    sending html template emails

--- 13-scripting/email_sender.py
+++ 13-scripting/email_sender.py
@@ -1,11 +1,15 @@
+from importlib.resources import path
 import smtplib
 from email.message import EmailMessage
+from string import Template
+from pathlib import Path
 
+html = Template(Path('index.html').read_text())
 email = EmailMessage()
 email['from'] = 'Omar Hamad'
 email['to'] = ''
 email['subject'] = 'This is a test'
-email.set_content('This is a test')
+email.set_content(html.substitute({'name': 'Omar'}), 'html')
 
 
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
